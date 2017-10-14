3bc4b21e1727787d8ffa07158b1b152622de16e3
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-10-15 00:07:47 +0300

    ok

--- Глава 1/1.3/main.c
+++ Глава 1/1.3/main.c
@@ -2,3 +2,18 @@
 // Created by Oleg Bukatchuk on 14/10/2017.
 //
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    const double pi=3.141592;
+    double R=3;
+    double S,L = 2 * pi * R;
+
+    S = pi * R * R;
+
+    printf("Длина окружности радиуса %.2f: %f\n", R, L);
+    printf("Площадь круга радиуса %.2f: %f\n", R, S);
+
+    return 0;
+}
--- Глава 1/1.4/main.c
+++ Глава 1/1.4/main.c
@@ -2,3 +2,23 @@
 // Created by Oleg Bukatchuk on 14/10/2017.
 //
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    char first = 'A', last = 'Z';
+    char x;
+    int count;
+
+    count = last - first + 1;
+    x = first + 10;
+
+    printf("Первый символ: %c\n", first);
+    printf("Код первого символа: %d\n", first);
+    printf("Последний символ: %c\n", last);
+    printf("Код последнего символа: %d", last);
+    printf("Колиество букв: %d\n", count);
+    printf("Символ \'%c\' с кодом %d\n", x, x);
+
+    return 0;
+}
