84c59f29922c83d5c08bee9548e12f5f001299d9
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-10-15 18:45:33 +0300

    ok

---
+++ Глава 1/1.17/main.c
@@ -0,0 +1,38 @@
+//
+// Created by Oleg Bukatchuk on 15/10/2017.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+    // Размер массива
+    const int n = 12;
+
+    // Инициализация массива
+    int pwr[n];
+
+    // Значение первого элемента в массиве
+    pwr[0] = 1;
+
+    printf("| %d |", pwr[0]);
+
+    // Индексная переменная
+    int k = 1;
+
+    while (k < n) {
+
+        // Значение элемента массива
+        pwr[k] = pwr[k - 1] * 2;
+
+        printf(" %d |", pwr[k]);
+
+        k++;
+    }
+
+    printf("\n");
+
+
+    return 0;
+}
