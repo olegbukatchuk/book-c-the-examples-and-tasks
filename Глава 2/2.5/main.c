ec4b845d60d27c01c2b5c880636f9b0f6a4b7b31
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-10-18 22:55:39 +0300

    ok

---
+++ Глава 2/2.5/main.c
@@ -0,0 +1,52 @@
+//
+// Created by Oleg Bukatchuk on 17/10/2017.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+// Кусочно-гладкая функция
+double f(double x) {
+
+    if (x<-1) {
+
+        return 0;
+
+    }
+
+    if (x<0) {
+
+        return 1-x*x;
+
+    }
+
+    if (x<2) {
+
+        return 1;
+
+    } else {
+
+        return exp(2-x);
+
+    }
+
+}
+
+int main(void) {
+
+    printf("x\t| f(x)\n");
+
+    printf("---------------------\n");
+
+    double x;
+
+    for (x=-1.75;x<5;x=x+0.5) {
+
+        printf("%.2f\t| %.4f\n",x,f(x));
+
+    }
+
+    return 0;
+
+}
---
+++ Глава 2/2.6/main.c
@@ -0,0 +1,84 @@
+//
+// Created by Oleg Bukatchuk on 17/10/2017.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Функция определения дня по его номеру
+void findDay(int num) {
+
+    printf("Номер дня в неделе: %d\n",num);
+
+    // Символьный массив для записи названия дня
+    char name[20];
+
+    // Опреатор выбора для определения дня
+    switch (num) {
+
+        case 1:
+
+            strcpy(name,"понедельник");
+
+            break;
+
+        case 2:
+
+            strcpy(name,"вторник");
+
+            break;
+
+        case 3:
+
+            strcpy(name,"среда");
+
+            break;
+
+        case 4:
+
+            strcpy(name,"четверг");
+
+            break;
+
+        case 5:
+
+            strcpy(name,"пятница");
+
+            break;
+
+        case 6:
+
+            strcpy(name,"суббота");
+
+            break;
+
+        case 7:
+
+            strcpy(name,"воскресенье");
+
+            break;
+
+        default:
+
+            strcpy(name,"нет такого дня");
+    }
+
+    printf("День недели: %s\n",name);
+
+}
+
+int main(void) {
+
+    int k;
+
+    // Определяем навание дня (через функцию)
+    for (k=0;k<=8;k++) {
+
+        findDay(k);
+
+    }
+
+    return 0;
+
+}
