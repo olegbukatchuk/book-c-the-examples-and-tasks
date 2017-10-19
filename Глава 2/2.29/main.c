4db6254bf0855b622c79392dbf0f8c461dc71cde
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-10-19 17:59:19 +0300

    ok

---
+++ Глава 2/2.29/main.c
@@ -0,0 +1,41 @@
+//
+// Created by Oleg Bukatchuk on 19/10/2017.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Функция определяющая уравнение
+double f(double x) {
+
+    return x*x-4*x-5;
+
+}
+// Производная
+double df(double x) {
+
+    return 2*x-4;
+
+}
+
+int main(void) {
+
+    // Переменная для записи корня
+    double x=7;
+
+    // Количество итераций
+    int n=10;
+
+    // Вычисление корня
+    while (n) {
+
+        x=x-f(x)/df(x);
+        n--;
+
+    }
+
+    printf("Решение уравнения: %f\n",x);
+
+    return 0;
+
+}
