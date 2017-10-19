d5b719a3a4af683feeeaa9ddae2a3892fef7cffb
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-10-19 15:14:12 +0300

    ok

---
+++ Глава 2/2.19/main.c
@@ -0,0 +1,24 @@
+//
+// Created by Oleg Bukatchuk on 19/10/2017.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+    int n=10;
+
+    int k,s=0;
+
+    for (k=1;k<=n;k++) {
+
+        s+=2*k-1;
+
+    }
+
+    printf("Сумма %d нечётных чисел равна %d\n",n,s);
+
+    return 0;
+
+}
