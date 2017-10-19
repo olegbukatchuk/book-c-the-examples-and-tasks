463a0e32a64fc4459cfd63595d4196dbed4d7bc2
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-10-19 14:58:49 +0300

    ok

---
+++ Глава 2/2.16/main.c
@@ -0,0 +1,33 @@
+//
+// Created by Oleg Bukatchuk on 19/10/2017.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+    // Количество слагаемых
+    int n;
+
+    // Считывание значения для количества слагаемых
+    printf("Сколько слагаемых в сумме? ");
+    scanf("%d",&n);
+
+    // Индексная переменная и переменная для записи суммы
+    int k=1,s=0;
+
+    while (1) {
+
+        s+=2*k-1;
+        k++;
+
+        if (k>n) break;
+
+    }
+
+    printf("%d\n",s);
+
+    return 0;
+
+}
---
+++ Глава 2/2.17/main.c
@@ -0,0 +1,31 @@
+//
+// Created by Oleg Bukatchuk on 19/10/2017.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+    // Количество слагаемых
+    int n;
+
+    // Считывание значения для количества слагаемых
+    printf("Сколько слагаемых в сумме? ");
+    scanf("%d",&n);
+
+    // k - индекс, s - сумма
+    int k=0,s=0;
+
+    do {
+
+        k++;
+        s+=2*k-1;
+
+    } while (k<n);
+
+    printf("Сумма %d нечётных чисел равна %d\n",s,n);
+
+    return 0;
+
+}
