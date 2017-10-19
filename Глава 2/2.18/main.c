977fb2d68eae3fdc06a6bc6f4c77faddcb3d0d9d
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-10-19 15:08:17 +0300

    ok

---
+++ Глава 2/2.18/main.c
@@ -0,0 +1,33 @@
+//
+// Created by Oleg Bukatchuk on 19/10/2017.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+    // Количество слагаемых
+    int n;
+
+    // Считываем значения
+    printf("Сколько слагаемых в сумме? ");
+    scanf("%d",&n);
+
+    printf("Сумма %d нечётных чисел равна ",n);
+
+    // Запись суммы
+    int s=0;
+
+    do {
+
+        s+=2*n-1;
+        n--;
+
+    } while (n);
+
+    printf("%d\n",s);
+
+    return 0;
+
+}
