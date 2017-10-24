e344f2c23c969269422364a831ca9f93522c3d27
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-10-24 15:22:09 +0300

    ok

---
+++ Глава 3/3.3/main.c
@@ -0,0 +1,31 @@
+//
+// Created by Oleg Bukatchuk on 24/10/2017.
+//
+
+// Использование указателя общего назначения
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+    // Указатель общего назначения
+    void *pnt;
+
+    int num;
+    double x;
+    char symb;
+
+    // Использование указателя
+    pnt=&num;
+    *((int*)pnt)=512;
+
+    printf("Число (тип int):\t%d\n",num);
+
+    pnt=&x;
+    *((double*)pnt)=1.234567;
+
+    printf("Число (тип int):\t%f\n",x);
+
+    return 0;
+
+}
