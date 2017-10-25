eaf2d0f5aec1e9551bd707aaccb71175ba1dfb9b
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-10-25 16:56:22 +0300

    ok

---
+++ Глава 3/3.13/main.c
@@ -0,0 +1,41 @@
+//
+// Created by Oleg Bukatchuk on 25/10/2017.
+//
+
+// Создание двумерного статического массива
+#include <stdio.h>
+#include <stdlib.h>
+
+// Размеры массива
+#define ROWS 3
+#define COLS 5
+
+int main(void) {
+
+    // Создание двумерного массива
+    int nums[ROWS][COLS];
+
+    // Индексные переменные и переменная-счётчик
+    int i,j,count=0;
+
+    // Заполнение массива и отображение значений элементов массива
+    for (i=0;i<ROWS;i++) {
+
+        for (j=0;j<COLS;j++) {
+
+            count++;
+
+            // Присваивание значения элементу
+            nums[i][j]=count;
+
+            printf("%3d",nums[i][j]);
+
+        }
+
+        printf("\n");
+
+    }
+
+    return 0;
+
+}
---
+++ Глава 3/3.14/main.c
@@ -0,0 +1,35 @@
+//
+// Created by Oleg Bukatchuk on 25/10/2017.
+//
+
+// Инициализация двумерного массива
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ROWS 3
+#define COLS 4
+
+int main(void) {
+
+    // Создание и инициализация двумерного массива
+    int nums[ROWS][COLS]={{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+
+    // Индексные переменные
+    int i,j;
+
+    // Отображение значений элементов массива
+    for (i=0;i<ROWS;i++) {
+
+        for (j=0;j<COLS;j++) {
+
+            printf("%3d ",nums[i][j]);
+
+        }
+
+    }
+
+    printf("\n");
+
+    return 0;
+
+}
