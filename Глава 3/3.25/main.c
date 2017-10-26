2f067da7d6b1c2c9ce1db4be3efd7f8bed1191c5
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-10-26 15:44:08 +0300

    ok

---
+++ Глава 3/3.25/main.c
@@ -0,0 +1,16 @@
+//
+// Created by Oleg Bukatchuk on 26/10/2017.
+//
+
+// Использование анонимных массивов
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+    // Индексная переменная
+    int k;
+
+    return 0;
+
+}
