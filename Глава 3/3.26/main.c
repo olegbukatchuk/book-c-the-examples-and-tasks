19f5caf50bb15555be5ec6fb0e61fbd7254fe6b9
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-10-26 16:14:49 +0300

    ok

--- Глава 3/3.26/main.c
+++ Глава 3/3.26/main.c
@@ -21,9 +21,9 @@ int main(void) {
 
     for (i=0;i<2;i++) {
 
-        for (j = 0; j < 3; j++) {
+        for (j=0;j<3;j++) {
 
-            printf("%d ", nums[i][j]);
+            printf("%d ",nums[i][j]);
 
         }
 
