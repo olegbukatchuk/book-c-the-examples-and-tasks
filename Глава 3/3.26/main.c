5b9478244284010007b1f92f4896803858470371
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-10-26 16:01:09 +0300

    ok

--- Глава 3/3.26/main.c
+++ Глава 3/3.26/main.c
@@ -2,3 +2,49 @@
 // Created by Oleg Bukatchuk on 26/10/2017.
 //
 
+// Анонимный двумерный массив
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+    // Индексные переменные
+    int i,j;
+
+    // Указатель на массив
+    int (*nums)[3];
+
+    // Присваивание значений указателю
+    nums=(int[2][3]){{1,2,3},{4,5,6}};
+
+    printf("Первый массив:\n");
+
+    for (i=0;i<2;i++) {
+
+        for (j = 0; j < 3; j++) {
+
+            printf("%d ", nums[i][j]);
+
+        }
+
+        printf("\n");
+
+    }
+
+    printf("Второй массив:\n");
+
+    for (i=0;i<3;i++) {
+
+        for (j=0;j<3;j++) {
+
+            printf("%d ",(int[][3]){{1,0,0},{0,1,0},0,0,1}[i][j]);
+
+        }
+
+        printf("\n");
+
+    }
+
+    return 0;
+
+}
---
+++ Глава 3/3.27/main.c
@@ -0,0 +1,5 @@
+//
+// Created by Oleg Bukatchuk on 26/10/2017.
+//
+
+//
