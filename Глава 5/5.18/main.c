5181f1c8de3bfc690dd1e2f8475032ae53a46eab
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-11-10 22:32:39 +0300

    ok

--- Глава 5/5.18/main.c
+++ Глава 5/5.18/main.c
@@ -19,6 +19,49 @@ union MyNumber getNumber(char x, int state)
     if (state)
         tmp.n = (int)(x + 10 - 'A');
     else {
-
+        switch (x) {
+            case 'A':
+                strcpy(tmp.s, "десять");
+                break;
+            case 'B':
+                strcpy(tmp.s, "одиннадцать");
+                break;
+            case 'C':
+                strcpy(tmp.s, "двенадцать");
+                break;
+            case 'D':
+                strcpy(tmp.s, "тринадцать");
+                break;
+            case 'E':
+                strcpy(tmp.s, "четырнадцать");
+                break;
+            case 'F':
+                strcpy(tmp.s, "пятнадцать");
+        }
     }
+
+    return tmp;
+}
+
+int main(void)
+{
+    /* Символьная переменная */
+    char k;
+
+    /* Отображение числовых значений */
+    for (k = 'A'; k <= 'F'; k++)
+        printf("%c - %d\n", k, getNumber(k, 1).n);
+
+    printf("-------------------------------\n");
+
+    /* Отображение текстовых значений */
+    for (k = 'A'; k <= 'F'; k++) {
+        /* Результат вызова функции присваивается значением экземпляру объединения */
+        obj = getNumber(k, 0);
+
+        /* Отображение текста из символьного массива */
+        printf("%c - %s\n", k, obj.s);
+    }
+
+    return 0;
 }
